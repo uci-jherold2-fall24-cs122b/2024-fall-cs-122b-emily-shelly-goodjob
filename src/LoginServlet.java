4551fef70e4c83f946702dc835dc315409629b9f
Shelly Wu <yiwenw29@uci.edu>
2024-10-26 17:14:37 -0700

    set log in page

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -4,6 +4,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
@@ -18,27 +25,46 @@ public class LoginServlet extends HttpServlet {
         /  in the real project, you should talk to the database to verify username/password
         */
         JsonObject responseJsonObject = new JsonObject();
-        if (username.equals("anteater") && password.equals("123456")) {
-            // Login success:
-
-            // set this user into the session
-            request.getSession().setAttribute("user", new User(username));
-
-            responseJsonObject.addProperty("status", "success");
-            responseJsonObject.addProperty("message", "success");
-
-        } else {
-            // Login fail
-            responseJsonObject.addProperty("status", "fail");
-            // Log to localhost log
-            request.getServletContext().log("Login failed");
-            // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
-            if (!username.equals("anteater")) {
-                responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
-            } else {
-                responseJsonObject.addProperty("message", "incorrect password");
+        try {
+            InitialContext initialContext = new InitialContext();
+            DataSource dataSource = (DataSource) initialContext.lookup("java:comp/env/jdbc/moviedb");
+            try (Connection conn = dataSource.getConnection()) {
+                if (!userExists(username, conn)) {
+                    responseJsonObject.addProperty("status", "fail");
+                    responseJsonObject.addProperty("message", "Username does not exist");
+                } else {
+                    if (validateUser(username, password, conn)) {
+                        request.getSession().setAttribute("user", new User(username));
+                        responseJsonObject.addProperty("status", "success");
+                        responseJsonObject.addProperty("message", "Log in successfully");
+                    } else {
+                        responseJsonObject.addProperty("status", "fail");
+                        responseJsonObject.addProperty("message", "Password does not match");
+                    }
+                }
             }
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+            responseJsonObject.addProperty("status", "fail");
+            responseJsonObject.addProperty("message", "Errors occurred while logging in");
         }
         response.getWriter().write(responseJsonObject.toString());
+    }
+
+    private boolean userExists(String email, Connection conn) throws SQLException {
+        try (PreparedStatement stmt = conn.prepareStatement("SELECT id FROM customers WHERE email = ?")) {
+            stmt.setString(1, email);
+            ResultSet rs = stmt.executeQuery();
+            return rs.next();
+        }
+    }
+
+    private boolean validateUser(String email, String password, Connection conn) throws SQLException {
+        try (PreparedStatement stmt = conn.prepareStatement("SELECT id FROM customers WHERE email = ? AND password = ?")) {
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            return rs.next();
+        }
     }
 }
